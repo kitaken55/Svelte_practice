ab91325399113c5a6abb56a471ebe01a0581b0de
kitaken55 <MiwaKentoDrums@yahoo.co.jp>
2020-06-27 23:47:28 +0900

    first init

---
+++ src/App.svelte
@@ -0,0 +1,52 @@
+<script>
+  let todoList = [
+    { id: 0, done: false, title: "レストランを予約する" },
+    { id: 1, done: false, title: "サプライズ用の指輪を買う" },
+    { id: 2, done: false, title: "フラッシュモブダンスを練習する" }
+  ];
+
+  let title = "";
+  function add() {
+    todoList = [
+      ...todoList,
+      {
+        id: todoList.length,
+        done: false,
+        title
+      }
+    ];
+  }
+
+   let condition = null
+
+  $: filteredTodoList = (todoList, condition) => {
+    return condition === null ? todoList : todoList.filter(t => t.done === condition)
+  }
+  
+</script>
+
+<style>
+  /* TODO: CSS得意なフレンズに頼む */
+  /* https://svelte.dev/tutorial/styling */
+</style>
+
+<div>
+  絞り込み:
+  <button on:click={() => { condition = null }}>すべて</button>
+  <button on:click={() => { condition = false }}>未完了</button>
+  <button on:click={() => { condition = true }}>完了</button>
+</div>
+<div>
+  <input type="text" bind:value={title} />
+  <button on:click={() => add()}>タスク追加</button>
+</div>
+<div>
+  <ul>
+    {#each filteredTodoList(todoList, condition) as todo (todo.id)}
+      <li>
+        <input type="checkbox" bind:checked={todo.done} />
+        {todo.title}
+      </li>
+    {/each}
+  </ul>
+</div>
